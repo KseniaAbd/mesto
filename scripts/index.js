5003aaf3501f2d2d56639ba3b928b94ed42b4ae0
Ksenia Abdulova <c.ijboldina@yandex.ru>
2022-12-13 22:42:45 +0300

    внести правки

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const editProfile = document.querySelector(".profile__edit-button_about");
+const popupOpened = document.querySelector(".popup_about");
+const popupClose = document.querySelector(".popup__close-button_about");
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input_userName");
+let jobInput = document.querySelector(".popup__input_userJob");
+let userName = document.querySelector(".profile__name");
+let userJob = document.querySelector(".profile__text");
+
+function openPopup() {
+  popupOpened.classList.add("popup_opened");
+  nameInput.value = userName.textContent;
+  jobInput.value = userJob.textContent;
+}
+editProfile.addEventListener("click", openPopup);
+function closePopup() {
+  popupOpened.classList.remove("popup_opened");
+}
+popupClose.addEventListener("click", closePopup);
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  userName.textContent = nameInput.value;
+  userJob.textContent = jobInput.value;
+  closePopup();
+}
+
+formElement.addEventListener("submit", handleFormSubmit);
